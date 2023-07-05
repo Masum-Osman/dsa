0ece12a70c989135aad12ef38b5944f0751fa5f5
Masum_Osman <ogkmasum@gmail.com>
2023-07-05 16:16:57 +0600

    LC 125 two pointer solutions

--- twoPointer/twoPointer.go
+++ twoPointer/twoPointer.go
@@ -30,3 +30,94 @@ func IsPalindrome(s string) bool {
 
 	return reversedStr == sanitizedStr
 }
+
+func IsPalindromeTwoPointer(s string) bool {
+	reg, err := regexp.Compile("[^A-Za-z0-9]+")
+	if err != nil {
+		return false
+	}
+
+	sanitizedStr := reg.ReplaceAllString(s, "")
+	sanitizedStr = strings.ToLower(sanitizedStr)
+
+	start := 0
+	end := len(sanitizedStr) - 1
+
+	for start < end {
+		if sanitizedStr[start] != sanitizedStr[end] {
+			return false
+		} else {
+			start++
+			end--
+		}
+	}
+
+	return true
+}
+
+func IsAlphaNumeric(chr byte) bool {
+	if (chr >= 48 && chr <= 57) ||
+		(chr >= 65 && chr <= 90) ||
+		(chr >= 97 && chr <= 122) {
+		return true
+	}
+	return false
+}
+
+func ToLowerCase(chr byte) byte {
+	if chr > 64 && chr < 91 {
+		chr = chr + 32
+	}
+
+	return chr
+}
+
+func IsPalindromeEx1(s string) bool {
+	for i, j := 0, len(s)-1; i < len(s) && j >= 0; {
+		chrLeft := s[i]
+		chrRight := s[j]
+
+		if !IsAlphaNumeric(chrLeft) {
+			i++
+			continue
+		}
+
+		if !IsAlphaNumeric(chrRight) {
+			j--
+			continue
+		}
+
+		if ToLowerCase(chrLeft) != ToLowerCase(chrRight) {
+			return false
+		}
+
+		i++
+		j--
+	}
+
+	return true
+}
+
+func IsPalindromeEx2(s string) bool {
+	var str strings.Builder
+	for i := range s {
+		if s[i] >= 65 && s[i] <= 90 {
+			str.WriteByte(s[i] + 32)
+		}
+		if s[i] >= 97 && s[i] <= 122 {
+			str.WriteByte(s[i])
+		}
+		if s[i] >= 48 && s[i] <= 57 {
+			str.WriteByte(s[i])
+		}
+	}
+	s = str.String()
+	l, r := 0, len(s)-1
+	for l <= r {
+		if s[l] != s[r] {
+			return false
+		}
+		l, r = l+1, r-1
+	}
+	return true
+}
--- twoPointer/twoPointer_test.go
+++ twoPointer/twoPointer_test.go
@@ -17,10 +17,31 @@ func TestIsPalindrome(t *testing.T) {
 	}
 
 	for i, tc := range cases {
-		got := IsPalindrome(tc.Input)
+		got := IsPalindromeTwoPointer(tc.Input)
 
 		if got != tc.Output {
 			t.Error("For test Case: ", i+1, "Wanted: ", tc.Output, " || Got: ", got)
+		}
+	}
+}
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	type TestCases struct {
+		Input  string
+		Output bool
+	}
+
+	cases := []TestCases{
+		{"A man, a plan, a canal: Panama", true},
+		{"race a car", false},
+		{" ", true},
+	}
+
+	for i, tc := range cases {
+		got := IsPalindromeEx2(tc.Input)
+
+		if got != tc.Output {
+			b.Error("For test Case: ", i+1, "Wanted: ", tc.Output, " || Got: ", got)
 		}
 	}
 }
